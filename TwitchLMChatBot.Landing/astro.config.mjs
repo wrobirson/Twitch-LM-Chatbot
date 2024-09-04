a7ac1b08f32bd58f1d9aee7fd969401efb20e7fe
Wender Robirson Batista Perez <wrobirson@outlook.com>
2024-09-03 23:31:38 -0400

    docs: Correct documentation build process
    - Fixed issues in the documentation build configuration for proper generation and deployment.

--- TwitchLMChatBot.Landing/astro.config.mjs
+++ TwitchLMChatBot.Landing/astro.config.mjs
@@ -6,7 +6,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://wrobirson.github.io/",
-  base: 'twitch-lm-chatbot',
   outDir: './../docs',
 });
