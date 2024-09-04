a8a2b55b2140cf38251d32d40bd8cbc904c09fc5
Wender Robirson Batista Perez <wrobirson@outlook.com>
2024-09-03 22:33:43 -0400

    fix(docs): correct Astro configuration for proper base path handling
    - Adjusted `astro.config.mjs` to use the correct `base` path for subdirectory deployment.

--- TwitchLMChatBot.Landing/astro.config.mjs
+++ TwitchLMChatBot.Landing/astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  base: '/wrobirson.github.io/twitch-lm-chatbot/',
+  outDir: './../docs'
 });
